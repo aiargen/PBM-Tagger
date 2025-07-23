6c9bdda85c581003e9799255f7de4ad55889322c
Chabukswar <achabuks@bmc.com>
2025-07-23 19:56:54 +0530

    .gitignore added and vocab content

---
+++ pbm/vocab.py
@@ -0,0 +1,22 @@
+# Controlled Vocabularies
+
+# pbm/vocab.py
+PRODUCTS = [
+    "DWP", "DWPC", "SRM"
+    ]
+
+ISSUE_CATEGORIES = [
+    "Customization","Configuration","Network","Restart","Data","Hotfix","Performance",
+    "Approval","Customer","Defect","Workflow","Cache","Report","Request","SRD",
+    "Entitlement","Certificate","Infra","Connector","Integration","Educate","Permissions",
+    "RestAPI","Email","Survey","Broadcast","Upgrade","Multitenancy","Search","Vulnerability",
+    "RSSO","Password","Virtual Marketplace","Roles","Collaborators","Studio Page","Branding",
+    "Fulfillment Request","Database","Notification Template","User Sync","Email Engine",
+    "Installation","Browse Categories","Migration"
+    # Add more if your slide continues
+    ]
+
+ACTION_CATEGORIES = [
+    "FAQ","R&D","Customization","KA","NA","Educate","Idea","Data"
+    # Expand if you have more internal values
+    ]
