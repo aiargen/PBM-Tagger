228db782b893e8850b3f9f4ebd79f50914df7fbf
Chabukswar <achabuks@bmc.com>
2025-07-24 20:07:29 +0530

    input file and input_path+read_html corrected

--- run.py
+++ run.py
@@ -5,11 +5,12 @@ from dotenv import load_dotenv;
 
 load_dotenv()
 
-INPUT_PATH = "data/cases_input.xlsx"
+INPUT_PATH = "data/input_data.xlsx"
 OUTPUT_PATH = "data/cases_output_with_pbm.xlsx"
 
 def main():
-    df = pd.read_excel(INPUT_PATH)
+    df = pd.read_html(INPUT_PATH)
+    df = df[0]
     df_out = process_dataframe(df, use_llm=True)  # set False for rule-only
     df_out.to_excel(OUTPUT_PATH, index=False)
     print(f"Saved: {OUTPUT_PATH}")
